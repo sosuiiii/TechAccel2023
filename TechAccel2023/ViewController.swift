33b7254a939b818f0e5c0a4f001c0747bb373865
tanaka_soushi <tanaka_soshi@cyberagent.co.jp>
2023-09-08 00:14:31 +0900

    first commit

---
+++ TechAccel2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechAccel2023
+//
+//  Created by 田中 颯志 on 9/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TechAccel2023UITests/TechAccel2023UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TechAccel2023UITestsLaunchTests.swift
+//  TechAccel2023UITests
+//
+//  Created by 田中 颯志 on 9/8/23.
+//
+
+import XCTest
+
+final class TechAccel2023UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
