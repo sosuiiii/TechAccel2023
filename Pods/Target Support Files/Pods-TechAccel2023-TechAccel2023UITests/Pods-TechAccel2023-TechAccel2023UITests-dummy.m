f4b509fbab79f19b1197d1bf473f87c6c5616ad1
tanaka_soushi <tanaka_soshi@cyberagent.co.jp>
2023-09-09 10:07:11 +0900

    not ignore diff

---
+++ Pods/Target Support Files/Pods-TechAccel2023-TechAccel2023UITests/Pods-TechAccel2023-TechAccel2023UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TechAccel2023_TechAccel2023UITests : NSObject
+@end
+@implementation PodsDummy_Pods_TechAccel2023_TechAccel2023UITests
+@end
---
+++ Pods/Target Support Files/Pods-TechAccel2023/Pods-TechAccel2023-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TechAccel2023 : NSObject
+@end
+@implementation PodsDummy_Pods_TechAccel2023
+@end
