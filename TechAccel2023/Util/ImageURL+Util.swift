ab494c250903fdbb005edd9a001b3b8d986bdb9c
tanaka_soushi <tanaka_soshi@cyberagent.co.jp>
2023-09-10 11:15:44 +0900

    Add Pokemon-Back image

--- TechAccel2023/Util/ImageURL+Util.swift
+++ TechAccel2023/Util/ImageURL+Util.swift
@@ -12,8 +12,12 @@ enum ImageURL {
         // 1.フシギダネ,2.フシギソウ,3.フシギバナ
         // 4.ヒトカゲ,5.リザード,6.リザードン
         // 7.ゼニガメ,8.カメール,9.カメックス, 10...1010
-        static func urlString(_ id: Int8) -> String {
+        static func frontUrlString(_ id: Int8) -> String {
             "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(id).png"
+        }
+
+        static func backUrlString(_ id: Int8) -> String {
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/\(id).png"
         }
     }
     enum Item {
